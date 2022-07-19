3ae6fd5cb2a6bed66e9798f462a04434e32fc34d
sleepyfran <fgzv@outlook.com>
2022-07-19 16:06:32 +0200

    :bug: Fix watchOS no data bug
    That was an obvious one in retrospective, wasn't it? :)

--- MabyWatch Watch App/MabyWatchApp.swift
+++ MabyWatch Watch App/MabyWatchApp.swift
@@ -1,10 +1,15 @@
+import Factory
+import MabyKit
 import SwiftUI
 
 @main
 struct MabyWatch_Watch_AppApp: App {
+    @Injected(Container.container) private var persistentContainer
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistentContainer.viewContext)
         }
     }
 }
